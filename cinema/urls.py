1284ba778119ccec657c0baa7735e05c52e606bb
Bohdan Bastiuk <bbastyuk@gmail.com>
2025-02-03 20:11:15 +0100

    Solution fix quotes

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,19 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from cinema.views import (GenreList, GenreDetail,
-                          ActorList, ActorDetail,
-                          CinemaHallViewSet, MovieViewSet)
+from cinema.views import GenreList, GenreDetail, ActorList, ActorDetail, CinemaHallViewSet, MovieViewSet
 
 router = DefaultRouter()
-router.register('cinema_halls', CinemaHallViewSet, basename='cinema_halls')
-router.register('movies', MovieViewSet, basename='movies')
+router.register("cinema_halls", CinemaHallViewSet, basename="cinema_halls")
+router.register("movies", MovieViewSet, basename="movies")
 
 urlpatterns = [
-    path('genres/', GenreList.as_view(), name='genre-list'),
-    path('genres/<int:pk>/', GenreDetail.as_view(), name='genre-detail'),
-    path('actors/', ActorList.as_view(), name='actor-list'),
-    path('actors/<int:pk>/', ActorDetail.as_view(), name='actor-detail'),
-    path('', include(router.urls)),
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("", include(router.urls)),
 ]
 
 app_name = "cinema"
